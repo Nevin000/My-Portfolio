cc5133d23253d6578fbab72519a90be12f3977ae
Nevin000 <nevinjeesara18@gmail.com>
2024-09-25 08:35:50 +0530

    Complete

--- script.js
+++ script.js
@@ -60,4 +60,83 @@ circles.forEach((elem) => {
 });
 
 // mix it up portfolio section//////
-var mixer = mixitup('.portfolio-gallery');
+var mixer = mixitup(".portfolio-gallery");
+
+// Active Menu /////////
+let menuLi = document.querySelectorAll("header ul li a");
+let section = document.querySelectorAll("section");
+
+function activeMenu() {
+  let len = section.length;
+  while (--len && window.scrollY + 97 < section[len].offsetTop) {
+    menuLi.forEach((sec) => sec.classList.remove("active"));
+    menuLi[len].classList.add("active");
+  }
+}
+
+activeMenu();
+window.addEventListener("scroll", activeMenu);
+
+// Sricky navbar /////////
+
+const header = document.querySelector("header");
+window.addEventListener("scroll", function () {
+  header.classList.toggle("sticky", window.scrollY > 50);
+});
+
+function sendEmail(event) {
+  event.preventDefault(); // Prevent the default form submission
+
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const address = document.getElementById("address").value;
+  const phone = document.getElementById("phone").value;
+  const message = document.getElementById("message").value;
+
+  const mailtoLink = `mailto:nevinjeesara18@gmail.com?subject=Contact For My Future Project&body=Name: ${encodeURIComponent(
+    name
+  )}%0D%0AEmail: ${encodeURIComponent(
+    email
+  )}%0D%0AAddress: ${encodeURIComponent(
+    address
+  )}%0D%0APhone: ${encodeURIComponent(
+    phone
+  )}%0D%0AMessage: ${encodeURIComponent(message)}`;
+
+  window.location.href = mailtoLink; // Open the mail client with the generated mailto link
+}
+
+// Toggle icon navbar /////////
+
+let menuIcon = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navlist.classList.toggle("open");
+};
+
+window.onscroll = () => {
+  menuIcon.classList.remove("bx-x");
+  navlist.classList.remove("open");
+};
+
+// parollax /////////
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show-items");
+    } else {
+      entry.target.classList.remove("show-items");
+    }
+  });
+});
+
+const scrollScale = document.querySelectorAll(".scroll-scale");
+scrollScale.forEach((el) => observer.observe(el));
+
+const scrollBottom = document.querySelectorAll(".scroll-bottom");
+scrollBottom.forEach((el) => observer.observe(el));
+
+const scrollTop = document.querySelectorAll(".scroll-top");
+scrollTop.forEach((el) => observer.observe(el));
